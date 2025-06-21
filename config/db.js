c12495ead985a45841778e580f6ff94444edfeb1
Sunil-Kumar380 <taakboy8484@gmail.com>
2025-06-21 13:14:31 +0530

    final commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const {Sequelize}=require('sequelize');
 require('dotenv').config();
 const db=new Sequelize(process.env.MYSQL_DATABASE,process.env.MYSQLUSER,process.env.MYSQL_ROOT_PASSWORD,{
     host:process.env.MYSQLHOST,
-    dialect:process.env.dialect
+    dialect:mysql
 })
 
 module.exports=db;
